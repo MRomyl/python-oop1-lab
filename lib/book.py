9158850e641a63c597277bbc3eba856f879a9ae8
MRomyl <romyl.martushev@gmail.com>
2025-11-04 14:26:42 -0800

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count: int):
+        self.title = title
+        self.page_count = page_count
+
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        self.page_count += 1
+        print("Flipping the page...wow, you read fast!")
     
-        
+
+
+"""
+Create Feature Branch
+
+Create Book class:
+__init__:
+title: Require user to input
+page_count: Require user to input
+Properties:
+page_count:
+Ensure it is an integer
+if not print “page_count must be an integer”
+Methods:
+turn_page():
+Will print “Flipping the page...wow, you read fast!
+
+”"""
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    valid_sizes = ["Small", "Medium", "Large"]
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+        if size not in Coffee.valid_sizes:
+            print("size must be Small, Medium, or Large")
+    
+    def tip(self):
+        self.price =+ 1
+        print("This coffee is great, here's a tip!")
+
+
+    
+
+
+
+
+
+"""
+__init__:
+size: Require user to input
+price: Require user to input
+Properties:
+Size
+Ensure size is either Small, Medium, or Large
+If not print “size must be Small, Medium, or Large”
+Methods:
+tip():
+Will print “This coffee is great, here’s a tip!”
+Will increase price by 1 
+
+"""
